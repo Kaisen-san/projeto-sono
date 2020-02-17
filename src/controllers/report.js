110476ccd347694b80596d6d10c4826d7b3b8c10
Felipe Andrade <fasa.work@gmail.com>
2020-02-17 14:41:24 -0300

    Improve report main page

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -1,10 +1,32 @@
 const db = require('../config/database')
 const { ERROR_MESSAGES } = require('../utils/enums')
 
-const renderMain = (req, res, next) => {
-  const { cookie, payload } = req.token
+const renderMain = async (req, res, next) => {
+  const { cookie } = req.token
 
-  res.status(200).render('report-main', { cookie: cookie })
+  try {
+    const years = db
+      .distinct('year')
+      .from('forms')
+
+    const states = db
+      .distinct('state')
+      .from('results')
+
+    res.status(200).render('report-main', {
+      cookie: cookie,
+      years: (await years.map(({ year }) => year)).sort(),
+      states: (await states.map(({ state }) => state)).sort()
+    })
+  } catch (e) {
+    console.error(e)
+    res.status(200).render('report-main', {
+      cookie: cookie,
+      years: [],
+      states: [],
+      acceptTerms: []
+    })
+  }
 }
 
 const renderLogin = (req, res, next) => {
@@ -22,69 +44,90 @@ const generateReport = async (req, res, next) => {
     })
   }
 
-  const form = await db
-    .select({
-      id: 'id',
-      year: 'year'
+  try {
+    let form = null
+
+    if (year !== -1) {
+      form = await db
+      .select({
+        id: 'id',
+        year: 'year'
+      })
+      .from('forms')
+      .where({ year: year })
+      .first()
+    } else {
+      form = await db
+      .select({
+        id: 'id',
+        year: 'year'
+      })
+      .from('forms')
+      .orderBy('year', 'desc')
+      .first()
+    }
+
+    if (form == null) {
+      return res.status(404).json({
+        error: ERROR_MESSAGES.INVALID_FORM_YEAR
+      })
+    }
+
+    const headers = await db
+      .select({ report: 'report' })
+      .from('form_questions')
+      .leftJoin('questions', 'id', 'id_question')
+      .where({ id_form: form.id })
+
+    const answersFilter = {
+      id_form: form.id
+    }
+
+    let reportNameState = 'TodosEstados'
+
+    if (state !== '-1') {
+      answersFilter.state = state
+      reportNameState = state
+    }
+
+    let reportNameTerms = 'AmbosTermos'
+
+    if (acceptTerms !== -1) {
+      answersFilter.accept_terms = acceptTerms
+      reportNameTerms = acceptTerms === 1 ? 'AceitouTermos' : 'NaoAceitouTermos'
+    }
+
+    const answers = await db
+      .select({
+        id: 'id',
+        acceptTerms: 'accept_terms',
+        year: 'year',
+        report: 'report'
+      })
+      .from('results')
+      .where(answersFilter)
+      .orderBy('id', 'asc')
+
+    const reportHeaders = `ID;AnoQuestionario;AnoResposta;Concorda${headers.reduce((acc, cur) => `${acc};${cur.report}`, '')}`
+    const reportAnswers = answers.reduce((acc, cur) => `${acc}${cur.id};${form.year};${cur.year};${Number(cur.acceptTerms)};${cur.report}\n`, '')
+    const reportContent = `${reportHeaders}\n${reportAnswers}`
+    const reportName = `Report_${form.year}_${reportNameState}_${reportNameTerms}.csv`
+
+    res.setHeader('Cache-Control', 'no-cache')
+    res.setHeader('Content-Type', 'text/csv; charset=utf-8')
+    res.setHeader('Content-Length', reportContent.length)
+    res.setHeader('Content-Disposition', `attachment; filename=${reportName}`)
+    res.status(200).send(reportContent)
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({
+      error: ERROR_MESSAGES.INTERNAL_SERVER_ERROR
     })
-    .from('forms')
-    .where({ year: year })
-    .first()
-
-  if (form == null) {
-    return res.status(404).json({
-      error: ERROR_MESSAGES.INVALID_FORM_YEAR
-    })
   }
-
-  const headers = await db
-    .select({ report: 'report' })
-    .from('form_questions')
-    .leftJoin('questions', 'id', 'id_question')
-    .where({ id_form: form.id })
-
-  const answersFilter = {
-    id_form: form.id
-  }
-
-  let reportNameState = 'TodosEstados'
-
-  if (state !== 'ALL') {
-    answersFilter.state = state
-    reportNameState = state
-  }
-
-  let reportNameTerms = 'AmbosTermos'
-
-  if (acceptTerms !== -1) {
-    answersFilter.accept_terms = acceptTerms
-    reportNameTerms = acceptTerms === 1 ? 'AceitouTermos' : 'NaoAceitouTermos'
-  }
-
-  const answers = await db
-    .select({
-      id: 'id',
-      acceptTerms: 'accept_terms',
-      year: 'year',
-      report: 'report'
-    })
-    .from('results')
-    .where(answersFilter)
-
-  const reportHeaders = `ID;AnoRelatorio;AnoResposta;Concorda${headers.reduce((acc, cur) => `${acc};${cur.report}`, '')}`
-  const reportAnswers = answers.reduce((acc, cur) => `${acc}${cur.id};${form.year};${cur.year};${Number(cur.acceptTerms)};${cur.report}\n`, '')
-  const reportContent = `${reportHeaders}\n${reportAnswers}`
-  const reportName = `Report_${form.year}_${reportNameState}_${reportNameTerms}.csv`
-
-  res.setHeader('Cache-Control', 'no-cache')
-  res.setHeader('Content-Type', 'text/csv; charset=utf-8')
-  res.setHeader('Content-Length', reportContent.length)
-  res.setHeader('Content-Disposition', `attachment; filename=${reportName}`)
-  res.status(200).send(reportContent)
 }
 
 module.exports = {
   renderMain,
   renderLogin,
-  generateReport,
+  generateReport
 }
