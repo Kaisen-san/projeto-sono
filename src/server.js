cdcb75f9f2ea8d52fe12c785830c935a7070d86f
Felipe Andrade <fasa.work@gmail.com>
2020-02-17 22:31:03 -0300

    Change public folder directory

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
