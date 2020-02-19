77091b13c0943c105b7bd033150b567c60a0bbb5
Felipe Andrade <fasa.work@gmail.com>
2020-02-18 21:54:03 -0300

    Fix process port

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const formRouter = require('./routes/form')
 const apiv1Router = require('./routes/api-v1')
 
 const app = express()
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
